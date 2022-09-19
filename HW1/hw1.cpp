c68f118c0a30cbacb95cc4cbe8bb169d10d651cd
Eric <b10831020@mail.ntust.edu.tw>
2022-09-19 23:06:15 +0800

    get started with new homework 1

---
+++ HW1/hw1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main()
+{
+    int inputCount = 0;
+    const int maxInput = 10;
+    float sideA, sideB, sideC;
+
+    while(inputCount < maxInput){
+        // if 
+    }
+}
+
+bool getUserInput(float &out_a, float &out_b, float &out_c)
+{
+    std::cout << "Please input three values as the length of three sides" <<std::endl;
+    std::cin >> out_a >> out_b >> out_c;
+}
