06aff1da2b37b01596ba2735a5e6ca798094ce55
Eric <ericacccls@gmail.com>
2022-09-21 13:54:35 +0800

    working on HW1

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <cmath>     //pow(), sqrt()
 
+bool getUserInput(float *out_sides);
+float getArea(const float* sides);
 
+struct Inputs
+{
+    int inputIdx;
+    float sides[3];
+    float area;
+};
 
 int main()
 {
     int inputCount = 0;
-    const int maxInput = 10;
-    float sideA, sideB, sideC;
+    std::vector<Inputs> inputLog(10);
+    float sides[3];
 
-    while(inputCount < maxInput){
-        // if 
+    while(getUserInput(&sides[0])){
+        float area = getArea(&sides[0]);
+        inputLog.push_back(Inputs{inputCount, *sides, area});
+        inputCount ++;
     }
 }
 
-bool getUserInput(float &out_a, float &out_b, float &out_c)
+float getArea(const float* sides)
+{   //takes in the pointer to the first element of array and offset using index
+    float halfPerimeter = (sides[0] + sides[1] + sides[2]) / 2;
+    return sqrt(pow(sides[0]-halfPerimeter, 2) * pow(sides[1]-halfPerimeter, 2) * pow(sides[2]-halfPerimeter, 2));
+}
+
+bool getUserInput(float* out_sides)
 {
+    //takes in a pointer to an float array to store user inputs
+    //returns false and breaks loop when input is 0, 0, 0
     std::cout << "Please input three values as the length of three sides" <<std::endl;
-    std::cin >> out_a >> out_b >> out_c;
+    std::cin >> out_sides[0] >> out_sides[1] >> out_sides[2];
+    return !(out_sides == 0 && out_sides[1] == 0 && out_sides[2] == 0);
 }
