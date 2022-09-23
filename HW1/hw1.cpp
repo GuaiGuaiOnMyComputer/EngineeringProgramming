91726938ac97f5a43d10dd1fbc23261db6792ea7
Eric <b10831020@mail.ntust.edu.tw>
2022-09-23 10:00:16 +0800

    bug fix

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -62,6 +62,7 @@ int main()
     std::cout << "\tAnd the triangle with the largest area is " << inputLog[count-1]->_area << " with sides " << '(' << inputLog[count-1]->_sideA << ", " << inputLog[count -1]->_sideB << ", "  << inputLog[count-1]->_sideC << ')' << std::endl;
     for(auto ptr : inputLog) //delete all heap pointers to prevent memory leak
         delete ptr;
+    system("pause"); //press any key to continue
     return 0;
 }
 
@@ -79,13 +80,13 @@ bool getUserInput(int& out_sideA, int& out_sideB, int& out_sideC)
     //takes in an int reference to store user inputs
     //returns false and breaks loop when input is (0, 0, 0), receives negative numbers or a triangle cannot be formed
     std::cout << "Please input three values as the length of three sides" <<std::endl;
-    std::cout << '\t' << std::endl;
+    std::cout << '\t';
     std::cin >> out_sideA >> out_sideB >> out_sideC;
     if(out_sideA <= 0 || out_sideB <= 0 || out_sideC <= 0){
         std::cout << "At least one side is smaller or equal to 0, terminating program." << std::endl;
         return false;
     }
-    if(out_sideA+out_sideB<out_sideC || out_sideB+out_sideC<out_sideA || out_sideA+out_sideC<out_sideB){
+    if(out_sideA+out_sideB<=out_sideC || out_sideB+out_sideC<=out_sideA || out_sideA+out_sideC<=out_sideB){
         std::cout << "Incorrect side lengths for a triangle, terminating program" << std::endl;
         return false;
     }
