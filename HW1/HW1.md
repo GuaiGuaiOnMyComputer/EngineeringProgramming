0cd20c6de62a4504ec177d155e77d8a7e8d67bf1
Eric <ericacccls@gmail.com>
2022-09-24 05:26:08 +0800

    remove content in Inputs constructor

---
+++ HW1/HW1.md
@@ -0,0 +1,48 @@
+# Homework 1
+
+
+| 班級     |    學號   | 姓名    |
+| :---:    |   :---:   | :---:  | 
+| 四機械四乙| B10831020 | 吳宇昕  |
+>日期 9/24/2022
+
+## Task 1：三角形
+>[Sorce Code](/HW1/hw1.cpp)以及
+>[Replit網址](https://replit.com/join/aemuvrnzpi-b10831020)
+
+>手動輸入三邊長之終端機輸出
+>![手動輸入三邊長](/HW1/manual_console_output.png) 
+>* 自動判斷三邊長是否合適。若三邊長為0、負數或長度無法組成三角形，程式終止
+>* 當三邊長可組成三角形，計算並顯示其面積
+>* 可以接收無限多筆輸入
+>* 使用```while loop```
+
+
+## Task２：使用```vector```儲存數據
+>* 自動產生亂數作為三邊長
+>* 自定義```class Inputs```儲存單筆輸入之三邊長與其三角形面積，並以```vector<Inputs*>```儲存各比輸入
+>```c++
+>class Inputs
+>{
+>public:
+>    int inputIdx ,sideA ,sideB ,sideC;
+>    float area;
+>    Inputs(int _inputIdx, int _sideA, int _sideB, int _sideC, float _area):
+>        inputIdx(_inputIdx),
+>        sideA(_sideA),
+>        sideB(_sideB),
+>        sideC(_sideC),
+>        area(_area)
+>        {}
+>
+>    Inputs() = delete;
+>};
+>
+>```
+>* 使用```for loop```與‵```auto```印出所有資料
+>```cpp
+>for(auto ptr : inputLog){ //print all inputs in this session
+>        printf("\tInput #%d: sides(%d, %d, %d) with area %.2f\n", ptr->inputIdx, ptr->sideA, ptr->sideB, ptr->sideC, ptr->area);
+>```
+
+## Task3：外送員
--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -9,25 +9,19 @@ class Inputs;
 bool getInput(int& out_sideA, int& out_sideB, int& out_sideC, bool autoRandom);
 bool checkInput(const int& sideA, const int& sideB, const int& sideC);
 float getArea(const int& sideA, const int& sideB, const int& sideC);
-void printSortedResult(const std::vector<Inputs>& sessionLog);
-
 
 class Inputs
 {
 public:
     int inputIdx ,sideA ,sideB ,sideC;
     float area;
-    Inputs(int _sideA, int _sideB, int _sideC, float _area):
+    Inputs(int _inputIdx, int _sideA, int _sideB, int _sideC, float _area):
+        inputIdx(_inputIdx),
         sideA(_sideA),
         sideB(_sideB),
         sideC(_sideC),
         area(_area)
-        {
-            this->sideA = _sideA;
-            this->sideB = _sideB;
-            this->sideC = _sideC;
-            this->area = _area;
-        }
+        {}
 
     Inputs() = delete;
 };
@@ -45,7 +39,7 @@ int main()
 
     while(getInput(sideA, sideB, sideC, autoRandom)){
         float area = getArea(sideA, sideB, sideC);
-        inputLog.push_back(new Inputs(sideA, sideB, sideC, area));
+        inputLog.push_back(new Inputs(count, sideA, sideB, sideC, area));
         count ++;
     }
     if(count == 0){
@@ -58,7 +52,7 @@ int main()
     int i, j;
     for(i = 0; i<count-1; i++){
         for(j=0; j<count-i-1; j++){
-            if(inputLog[j]->area > inputLog[j+1]->area){ //this segfaults? WTF?
+            if(inputLog[j]->area > inputLog[j+1]->area){
                 Inputs tmp = *inputLog[j];
                 *inputLog[j] = *inputLog[j+1];
                 *inputLog[j+1] = tmp;
@@ -69,8 +63,12 @@ int main()
     printf("\tThe largest triangle's area is %.2f with sides (%d, %d, %d)\n", inputLog[0]->area, inputLog[0]->sideA, inputLog[0]->sideB, inputLog[0]->sideC);
     printf("\tThe smallest triangle's area is %.2f with sides (%d, %d, %d)\n", inputLog[count-1]->area, inputLog[count-1]->sideA, inputLog[count-1]->sideB, inputLog[count-1]->sideC);
     
-    for(auto ptr : inputLog) //delete all heap pointers to prevent memory leak
-        delete ptr;
+
+    printf("Printing all inputs in this session\n=======================================================\n");
+    for(auto ptr : inputLog){ //print all inputs in this session
+        printf("\tInput #%d: sides(%d, %d, %d) with area %.2f\n", ptr->inputIdx, ptr->sideA, ptr->sideB, ptr->sideC, ptr->area);
+        delete ptr; //deleting all heap-allocated pointers to prevent memory leak
+    }
     system("pause"); //press any key to continue
     return 0;
 }
@@ -117,38 +115,52 @@ bool checkInput(const int& sideA, const int& sideB, const int& sideC)
     return true;
 }
 
-
-
 /*Replit https://replit.com/join/aemuvrnzpi-b10831020
 Output MANUAL mode:
 Student B10831020
-Please choose whether to use automatically generated numbers as input?(1:Yes,
-0:No)  0 Please input three values as the length of three sides 9 3 7 The area
-of this triangle is 8.125 Please input three values as the length of three sides
-    11 22 15
-    The area of this triangle is 234
+Please choose whether to use automatically generated numbers as input?(1:Yes, 0:No)    0
 Please input three values as the length of three sides
-    8 7 6
-    The area of this triangle is 39.375
+    23 64 76
+    The area of this triangle is 5630.62
 Please input three values as the length of three sides
-    76 87 93
-    The area of this triangle is 74620
+    87 42 67
+    The area of this triangle is 19096
 Please input three values as the length of three sides
-    -1 8 2
-At least one side is smaller or equal to 0, terminating program.
-    The largest triangle's area is 8.12 with sides (9, 3, 7)
-    The smallest triangle's area is 74620.00 with sides (76, 87, 93)
-sh: 1: pause: not found
+    87 34 69
+    The area of this triangle is 12688
+Please input three values as the length of three sides
+    343 454 767
+    The area of this triangle is 2.15988e+06
+Please input three values as the length of three sides
+    23 54 77
+Incorrect side lengths for a triangle, terminating program
+    The largest triangle's area is 5630.62 with sides (23, 64, 76)
+    The smallest triangle's area is 2159880.00 with sides (343, 454, 767)
+Printing all inputs in this session
+=======================================================
+    Input #0: sides(23, 64, 76) with area 5630.62
+    Input #2: sides(87, 34, 69) with area 12688.00
+    Input #1: sides(87, 42, 67) with area 19096.00
+    Input #3: sides(343, 454, 767) with area 2159880.00
 
 AUTOMODE:
 Student B10831020
 Please choose whether to use automatically generated numbers as input?(1:Yes,
-0:No)    1 Generating side lengths from random numbers The sides are (4, 4, 3)
-The area of this triangle is 5.625 Generating side lengths from random numbers
-    The sides are (4, 3, 3) The area of this triangle is 4
+0:No)   1 Generating side lengths from random numbers The sides are (5, 5, 2)
+The area of this triangle is 4 Generating side lengths from random numbers The
+sides are (2, 2, 3) The area of this triangle is 1.125 Generating side lengths
+from random numbers The sides are (7, 7, 8) The area of this triangle is 48
 Generating side lengths from random numbers
-    The sides are (0, 0, 4)At least one side is smaller or equal to 0,
-terminating program. The largest triangle's area is 4.00 with sides (4, 3, 3)
-    The smallest triangle's area is 5.62 with sides (4, 4, 3)
+    The sides are (6, 6, 9) The area of this triangle is 30.375
+Generating side lengths from random numbers
+    The sides are (1, 1, 8)Incorrect side lengths for a triangle, terminating
+program The largest triangle's area is 1.12 with sides (2, 2, 3) The smallest
+triangle's area is 48.00 with sides (7, 7, 8) Printing all inputs in this
+session
+=======================================================
+    Input #1: sides(2, 2, 3) with area 1.12
+    Input #0: sides(5, 5, 2) with area 4.00
+    Input #3: sides(6, 6, 9) with area 30.38
+    Input #2: sides(7, 7, 8) with area 48.00
 sh: 1: pause: not found
 */
