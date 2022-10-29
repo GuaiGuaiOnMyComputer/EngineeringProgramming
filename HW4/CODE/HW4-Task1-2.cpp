6d90e7ca3dcac4aa07fce48ea724ea4ba76a8389
Eric <ericacccls@gmail.com>
2022-10-29 22:17:57 +0800

    duplicat value not fixed

--- HW4/CODE/HW4-Task1-2.cpp
+++ HW4/CODE/HW4-Task1-2.cpp
@@ -56,17 +56,66 @@ struct Range
 void getRangeInput(Range& out_range);
 void assignRandomValues(const Range& rng, CompositeMatrixCplx& m1, CompositeMatrixCplx& m2);
 void printCompositeMatrix(const CompositeMatrixCplx& m, const char* msg);
+void matrixAdd(const CompositeMatrixCplx& m1, const CompositeMatrixCplx& m2, CompositeMatrixCplx& out_result);
+void printNonDiaSum(const CompositeMatrixCplx& m);
 
 int main()
 {
     std::cout << "Student B10831020" << std::endl;
     Range userSetRange;
     CompositeMatrixCplx m1, m2, addResult;
-    getRangeInput(userSetRange);
+    // getRangeInput(userSetRange);
+    userSetRange.imgMin = 0.7;
+    userSetRange.imgMax = 75;
+    userSetRange.realMax = 2;
+    userSetRange.realMin = 10;
+
     assignRandomValues(userSetRange, m1, m2);
     printCompositeMatrix(m1, "Complex matrix m1"); 
+    printCompositeMatrix(m2, "Complex matrix m2");
+    matrixAdd(m1, m2, addResult);
+    printCompositeMatrix(addResult, "m1 + m2");
+    printNonDiaSum(addResult);
     system("pause");   
     return 0;
+}
+
+void printNonDiaSum(const CompositeMatrixCplx& m)
+{
+    for(int i=0; i<m.size(); i++){
+        for(int j=0; j<m[i].data.size(); j++){
+            if(i==j){
+                if(i == 3){
+                    std::cout << "----------" << '\t';
+                }
+                else{
+                    std::cout << "----------" << '\t';
+                }
+            }
+            else{
+                if(i==3){
+                    std::cout << m[i].data[j].real() << "+" << m[i].data[j].imag() << 'i' << '\t';
+                }
+                else{
+                    std::cout << m[i].data[j].real() << '+' << m[i].data[j].imag() << 'i' << '\t';
+                }
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void matrixAdd(const CompositeMatrixCplx& m1, const CompositeMatrixCplx& m2, CompositeMatrixCplx& out_result)
+{
+    out_result.reserve(4);
+    for(int i=0; i<m1.size(); i++){
+        out_result.push_back(CplxRow());
+        for(int j=0; j<m1.size(); j++){
+            float realAdd = m1[i].data[j].real() + m2[i].data[j].real();
+            float imgAdd = m1[i].data[j].imag() + m2[i].data[j].imag();
+            out_result[i].data.push_back(Complex(realAdd, imgAdd));
+        }
+    }
 }
 
 void printCompositeMatrix(const CompositeMatrixCplx& m, const char* msg)
@@ -90,47 +139,34 @@ void assignRandomValues(const Range& rng, CompositeMatrixCplx& m1, CompositeMatr
         m1.push_back(CplxRow()); // calls the CplxRow constructor and auto reserve 4 complexes in a row
         m2.push_back(CplxRow()); // checkout the CplxRow consturctor
         float randNumbers[4];
-        for(int q=0; q<4; q++)
+        for(int j=0; j<4; j++)
         {
-            randNumbers[q] = randGenerator();
-            float randOffsetCoeff = 1 + (randGenerator() - randGenerator.min()) / (float)(randGenerator.max() - randGenerator.min()); // the result will be 1.xxx
-            float distance;
+            // randNumbers[j] = (float)randGenerator(); this line seems necessary but it creates weird bug
+            float randOffsetCoeff = 1 + (float)(randGenerator() - randGenerator.min()) / (float)(randGenerator.max() - randGenerator.min()); // the result will be 1.xxx
             // left-shift or right-shift the generated random number into the user input range
-            switch (q%2)
+            switch (j%2)
             {
-                
                 case 1:
                     // handling the real part of a complex number if i is an odd number(if this current iteration is an odd iteration)
-                    if(randNumbers[q] > rng.realMax){
-                        distance = randOffsetCoeff * (randNumbers[q] - rng.realMax);
-                        float tmp = randNumbers[q];
-                        randNumbers[q] = tmp - distance;
+                    if(randNumbers[j] > rng.realMax){
+                        randNumbers[j] = randNumbers[j] - randOffsetCoeff * (randNumbers[j] - rng.realMax);
                     }
-                    else if(randNumbers[q] < rng.realMin){
-                        distance = randOffsetCoeff * (rng.realMin - randNumbers[q]);
-                        float tmp = randNumbers[q];
-                        randNumbers[q] = tmp + distance;
+                    else if(randNumbers[j] < rng.realMin){
+                        randNumbers[j] = randNumbers[j] + randOffsetCoeff * (rng.realMin - randNumbers[j]);
                     }
                     break;
                 case 0:
                     // handling the imaginary part of a complex number if i is an even number(if this current iteration is an even iteration)
-                    if(randNumbers[q] > rng.imgMax){
-                        distance = randOffsetCoeff * (randNumbers[q] - rng.imgMax);
-                        float tmp = randNumbers[q];
-                        randNumbers[q] = tmp - distance;
+                    if(randNumbers[j] > rng.imgMax){
+                        randNumbers[j] = randNumbers[j] - randOffsetCoeff * (randNumbers[j] - rng.imgMax);
                     }
-                    else if(randNumbers[q] < rng.imgMin){
-                        distance = randOffsetCoeff * (rng.imgMin - randNumbers[q]);
-                        float tmp = randNumbers[q];
-                        randNumbers[q] = tmp + distance;
+                    else if(randNumbers[j] < rng.imgMin){
+                        randNumbers[j] = randNumbers[j] + randOffsetCoeff * (rng.imgMin - randNumbers[j]);
                     }
                     break;
-                
             }
-        }
-        for(int _=0; _<4; _++){ // *2 to handle the real and imginary parts of each element
-            m1[i].data.push_back(std::complex<float> (randNumbers[0], randNumbers[1])); // calls the constructor of std::complex<float>
-            m2[i].data.push_back(std::complex<float> (randNumbers[2], randNumbers[3])); // push_back a complex number into the matrix
+            m1[i].data.push_back(Complex(randNumbers[0], randNumbers[1])); // calls the constructor of std::complex<float>
+            m2[i].data.push_back(Complex(randNumbers[2], randNumbers[3])); // push_back a complex number into the matrix
         }
         m1[i].GetAvg();
         m2[i].GetAvg();
