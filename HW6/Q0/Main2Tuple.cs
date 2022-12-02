3c95a8b16ec284020765761b8f61a219af2fd0f2
Eric <ericacccls@gmail.com>
2022-12-03 00:09:34 +0800

    update vscode working environment

--- HW6/Q0/Main2Tuple.cs
+++ HW6/Q0/Main2Tuple.cs
@@ -5,25 +5,29 @@ using System;
 using System.Diagnostics;  // for Assert( )
 
 class Program {
-  public static void Main (string[] args) {
-    string msg ="[觀-照-知-悟-證-行]::ME5015 工程領域的程式語言實務";
+  public static void Main (string[] args) 
+  {
     int[] userInput = new int[2];
-    Console.WriteLine (msg + " StuID = B10831020");
-    Console.WriteLine("[TUPLE DEMO] Two Numbers:111 220 is assumed");
-    HelloWorldMsg(msg); // New added. TimeStamp is shown.
-    Console.WriteLine("Please enter the first number");
-    userInput[0] = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Please enter the seconde number");
-    userInput[1] = Convert.ToInt32(Console.ReadLine());
-
+    HelloWorldMsg(); // New added. TimeStamp is shown.
+    GetUserInput(out userInput[0], out userInput[1]);
     Tuple<double, double> result = CalSumAndAvg(userInput);
     Console.WriteLine($"sum = {result.Item1}");
     Console.WriteLine($"avg = {result.Item2}");
-    // wait for your key to stop its exectution
-    Get2Values ((int)result.Item2); //casting is needed.
-    Get2Values ((int)result.Item1, 11); //TODO - why ?
-  //  Get2Values ((int)result.Item1, 3); //Try this -uncomment it
+    var sb = new System.Text.StringBuilder();
+    // Get2Values((int)result.Item2); //casting is needed.
+    // Get2Values((int)result.Item1, 11); //TODO - why ?
+    Get2Values ((int)result.Item1, 3, ref sb); //Try this -uncomment it
+      // result is a tuple holding 2 doubles, while the method Get2Falues is a function taking int as parameters
+    Console.Write(sb.ToString());
     Console.ReadKey();
+  }
+
+  public static void GetUserInput(out int n1, out int n2)
+  {
+    Console.WriteLine("Please enter the first number");
+    n1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please enter the seconde number");
+    n2 = Convert.ToInt32(Console.ReadLine());
   }
 
 // Check out the defintion of ValueTuple in c# 7.0 spec.
@@ -39,23 +43,26 @@ class Program {
     return new Tuple<double, double>(sum, avg);
   }
 // TODO: Added -- call this 
-  public static void HelloWorldMsg (string msg){
-     string now1 = DateTime.Now +"\n"+msg+"\n"; 
-     Console.WriteLine($"{now1}");
+  public static void HelloWorldMsg()
+  {
+    string msg ="[觀-照-知-悟-證-行]::ME5015 工程領域的程式語言實務";
+    string now1 = DateTime.Now +"\n"+msg+"\n"; 
+    Console.WriteLine("Student ID: B10831020");
+    Console.WriteLine($"{now1}");
   }	 
   
  //  GOAL:: GenerateRandomNumbers -- 
  //  TODO: make it complete as a new Method:
-  public static void Get2Values(int ceiling, int cCount=5 ){
+  public static void Get2Values(int ceiling, int cCount, ref System.Text.StringBuilder _sb){
     Random rnd = new Random();
     int inum1, inum2; 
     Debug.Assert(cCount >=5);  // Microsoft - MSDOCS
     
-    Console.WriteLine($"\nLoop count {cCount}\n") ; //TODO 
+    _sb.AppendLine($"\nLoop count {cCount}\n") ; //TODO 
     for (var idx =0; idx < cCount ;idx++) {
-      inum1 =  rnd.Next() % ceiling; 
-      inum2  = rnd.Next() % ceiling *10; 
-      Console.WriteLine("Loop= {2:G3}, {0:G4}, {1:G4}", inum1, inum2, idx );
+      inum1 = rnd.Next() % ceiling; 
+      inum2 = rnd.Next() % ceiling *10; 
+      _sb.AppendLine(String.Format("Loop= {2:G3}, {0:G4}, {1:G4}", inum1, inum2, idx ));
     }
   }
 		// 
